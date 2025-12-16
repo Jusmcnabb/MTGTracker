f2f624989280b826d04b6231c28656931448bae8
jusmcnabb <Jusmcnabb@gmail.com>
2025-12-16 14:56:31 -0600

    Initial commit

---
+++ src/main/java/com/tracker/MTGTracker/MtgTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tracker.MTGTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tracker/MTGTracker/MtgTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracker.MTGTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
