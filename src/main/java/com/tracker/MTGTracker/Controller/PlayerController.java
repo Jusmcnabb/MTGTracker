5d002b5807d2865a6b4614bb40b5d5564e19ed62
jusmcnabb <Jusmcnabb@gmail.com>
2025-12-16 20:55:48 -0600

    Added the controller classes, ready to merge.

--- src/main/java/com/tracker/MTGTracker/Controller/PlayerController.java
+++ src/main/java/com/tracker/MTGTracker/Controller/PlayerController.java
@@ -1,5 +1,20 @@
 package com.tracker.MTGTracker.Controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/players")
+@RestController
 public class PlayerController {
     
+
+    //Create a new player
+    @PostMapping
+    public String createPlayer (@RequestBody String name) {
+        return "Player " + name + " created";
+    }
+
+
 }
